14baff9549d4148f75aec72a975958498ff1c954
Spencer Rogers <spencer1248@gmail.com>
2013-10-28 11:30:00 -0400

    Clarify form tests

--- spec/004.forms/02_nested_forms_spec.rb
+++ spec/004.forms/02_nested_forms_spec.rb
@@ -1,58 +1,62 @@
 feature "nested forms" do
   context "hashes" do
-    describe "table attributes" do
-      # Expected params hash:
-      # {
-      #   table: {
-      #     name: 'Foo',
-      #     wood: 'Maple',
-      #     price: 1000
-      #   }
-      # }
+    context "views" do
+      describe "table_form.erb" do
+        # Expected params hash:
+        # {
+        #   table: {
+        #     name: 'Foo',
+        #     wood: 'Maple',
+        #     price: 1000
+        #   }
+        # }
 
-      it "should have 'name', 'wood', and 'price' keys under the 'table' key" do
-        name = "Kitchen Table"
-        wood = "Maple"
-        price = 1200
+        it "should have 'name', 'wood', and 'price' keys under the 'table' key" do
+          name = "Kitchen Table"
+          wood = "Maple"
+          price = 1200
 
-        visit "/table"
+          visit "/table"
 
-        # Supply input IDs for the these matchers to reference.
-        fill_in "name", with: name
-        fill_in "wood", with: wood
-        fill_in "price", with: price
+          # Supply input IDs for the these matchers to reference.
+          fill_in "name", with: name
+          fill_in "wood", with: wood
+          fill_in "price", with: price
 
-        click_button "Save"
+          click_button "Save"
 
-        expect(page).to have_content name.upcase
-        expect(page).to have_content wood.upcase
-        expect(page).to have_content price
+          expect(page).to have_content name.upcase
+          expect(page).to have_content wood.upcase
+          expect(page).to have_content price
+        end
       end
     end
   end
 
   context "arrays" do
-    describe "lamp colors" do
-      # Expected params hash:
-      # {
-      #   lamp: {
-      #     colors: ['red', 'blue']
-      #   }
-      # }
+    context "views" do
+      describe "lamp colors" do
+        # Expected params hash:
+        # {
+        #   lamp: {
+        #     colors: ['red', 'blue']
+        #   }
+        # }
 
-      it "should have 4 colors under the 'lamp' key (use IDs to help identify the fields)" do
-        colors = ['red', 'green', 'blue', 'yellow']
+        it "should have 4 colors under the 'lamp' key (use IDs to help identify the fields)" do
+          colors = ['red', 'green', 'blue', 'yellow']
 
-        visit "/lamp"
+          visit "/lamp"
 
-        # Supply input IDs for the these matchers to reference.
-        4.times do |n|
-          fill_in "color_#{n}", with: colors[n]
+          # Supply input IDs for the these matchers to reference.
+          4.times do |n|
+            fill_in "color_#{n}", with: colors[n]
+          end
+
+          click_button "Save"
+
+          expect(page).to have_content colors.join(',')
         end
-
-        click_button "Save"
-
-        expect(page).to have_content colors.join(',')
       end
     end
   end
