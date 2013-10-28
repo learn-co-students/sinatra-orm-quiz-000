229c3a3fe1d708851fd1c2a3a963fe289adead43
Spencer Rogers <spencer1248@gmail.com>
2013-10-28 00:47:51 -0400

    Remove copy/pasted tests

--- spec/003.sinatra/02_params_spec.rb
+++ spec/003.sinatra/02_params_spec.rb
@@ -14,22 +14,5 @@ describe Sinatra do
         last_response.body.should == URI.decode('%73%70%69%63%79%20%63%68%69%63%6B%65%6E')
       end
     end
-
-    describe "POST" do
-      it "handles POST to /" do
-         post '/'
-         expect(last_response.status).to eq(200)
-       end
-    end
-
-    describe "redirecting" do
-      it "should follow a redirect from /zig to /zag" do
-        get "/zig"
-        follow_redirect!
-
-        "http://example.org/zag".should == last_request.url
-        expect(last_response.status).to eq(200)
-      end
-    end
   end
 end
