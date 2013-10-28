cc01e8db4834f81d33f4c8844daaa2d8d594f4c1
Spencer Rogers <spencer1248@gmail.com>
2013-10-28 13:33:18 -0400

    Clarify

--- spec/004.forms/02_nested_forms_spec.rb
+++ spec/004.forms/02_nested_forms_spec.rb
@@ -1,40 +1,37 @@
 feature "nested forms" do
-  context "hashes" do
-    context "/views/" do
-      describe "table_form.erb" do
-        # Expected params hash:
-        # {
-        #   table: {
-        #     name: 'Foo',
-        #     wood: 'Maple',
-        #     price: 1000
-        #   }
-        # }
+  context "views" do
+    describe "table_form.erb" do
+      # Expected params hash:
+      # {
+      #   table: {
+      #     name: 'Foo',
+      #     wood: 'Maple',
+      #     price: 1000
+      #   }
+      # }
 
-        it "should have 'name', 'wood', and 'price' keys under the 'table' key" do
-          name = "Kitchen Table"
-          wood = "Maple"
-          price = 1200
+      it "should have 'name', 'wood', and 'price' keys under the 'table' key" do
+        name = "Kitchen Table"
+        wood = "Maple"
+        price = 1200
 
-          visit "/table"
+        visit "/table"
 
-          # Supply input IDs for the these matchers to reference.
-          fill_in "name", with: name
-          fill_in "wood", with: wood
-          fill_in "price", with: price
+        # Supply input IDs for the these matchers to reference.
+        fill_in "name", with: name
+        fill_in "wood", with: wood
+        fill_in "price", with: price
 
-          click_button "Save"
+        click_button "Save"
 
-          expect(page).to have_content name.upcase
-          expect(page).to have_content wood.upcase
-          expect(page).to have_content price
-        end
+        expect(page).to have_content name.upcase
+        expect(page).to have_content wood.upcase
+        expect(page).to have_content price
       end
     end
   end
 
-  context "arrays" do
-    context "/views/" do
+    context "views" do
       describe "lamp colors" do
         # Expected params hash:
         # {
