754aa5b1fb91858fd3e79565c66741d7edf8f339
Spencer Rogers <spencer1248@gmail.com>
2013-10-28 00:15:12 -0400

    Add Sinatra describe block

--- spec/003.sinatra/routing_spec.rb
+++ spec/003.sinatra/routing_spec.rb
@@ -1,32 +1,34 @@
 require 'spec_helper'
 require_relative 'app'
 
-describe "routing" do
-  def app
-    App
-  end
-
-  describe "GET" do
-   it "handles GET to /" do
-      get '/'
-      expect(last_response.status).to eq(200)
+describe Sinatra do
+  describe "routing" do
+    def app
+      App
     end
-  end
 
-  describe "POST" do
-    it "handles POST to /" do
-       post '/'
-       expect(last_response.status).to eq(200)
-     end
-  end
+    describe "GET" do
+     it "handles GET to /" do
+        get '/'
+        expect(last_response.status).to eq(200)
+      end
+    end
 
-  describe "redirecting" do
-    it "should follow a redirect from /zig to /zag" do
-      get "/zig"
-      follow_redirect!
+    describe "POST" do
+      it "handles POST to /" do
+         post '/'
+         expect(last_response.status).to eq(200)
+       end
+    end
 
-      "http://example.org/zag".should == last_request.url
-      expect(last_response.status).to eq(200)
+    describe "redirecting" do
+      it "should follow a redirect from /zig to /zag" do
+        get "/zig"
+        follow_redirect!
+
+        "http://example.org/zag".should == last_request.url
+        expect(last_response.status).to eq(200)
+      end
     end
   end
 end
