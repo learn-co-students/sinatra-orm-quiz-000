84e7e27ced452b87791c230e90318b23a6d15be6
Spencer Rogers <spencer1248@gmail.com>
2013-10-29 09:00:51 -0400

    Fix numerical fields for crud read

--- spec/002.ORM/01_crud_spec.rb
+++ spec/002.ORM/01_crud_spec.rb
@@ -51,7 +51,8 @@ describe "ORMs" do
         # TODO: Get these to pass using the instance of campaign
         expect(__).to eq("Soylent")
         expect(__).to eq("Free your body.")
-        expect(__).to eq("10000000")
+        expect(__).to eq(10000000)
+        expect(__).to eq(500000)
         expect(__).to eq(Time.new(2013, 6, 15))
         expect(__).to eq(Time.new(2013, 8, 30))
       end
