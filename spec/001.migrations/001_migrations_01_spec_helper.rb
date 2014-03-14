d58f760d655c54b2cf06bd068308cbc41e06c61c
Logan Hasson <logan.hasson@gmail.com>
2014-03-14 11:56:02 -0400

    Check for migrations in spec helper files

--- spec/001.migrations/001_migrations_01_spec_helper.rb
+++ spec/001.migrations/001_migrations_01_spec_helper.rb
@@ -1,3 +1,7 @@
 ENV["QUIZ_ENV"] = "001_01_migrations"
 
 require 'spec_helper'
+
+if ActiveRecord::Migrator.needs_migration?
+  ActiveRecord::Migrator.migrate('spec/001.migrations/01_migrations')
+end
--- spec/002.ORM/002_orm_spec_helper.rb
+++ spec/002.ORM/002_orm_spec_helper.rb
@@ -2,3 +2,7 @@ ENV["QUIZ_ENV"] = "002_migrations"
 
 require 'spec_helper'
 require_relative './crowd_fundr_campaign'
+
+if ActiveRecord::Migrator.needs_migration?
+  ActiveRecord::Migrator.migrate('spec/002.ORM/migrations')
+end
